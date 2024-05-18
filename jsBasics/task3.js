dcb1ac0c30e2f78152db02ae6a2c76588571609c
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-05-18 16:57:05 +0300

    completed 1-4 tasks in JS basics

---
+++ jsBasics/task3.js
@@ -0,0 +1,22 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+readline.question('May I have your ID card, please? ', age => {
+    console.log(`I see, your age is ${age}`);
+    checkAge(age);
+    readline.close();
+})
+
+function checkAge(age){
+    if (age >= 18){
+        console.log('You are old enough to view this content')
+        return true;
+    }else{
+        console.log('Access denied, you are not old enough to view this content')
+        return false;
+    }
+
+}
