77dff96185530c4a3b6c5e2d8db5b929353140fd
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-05-18 22:11:12 +0300

    bug fixes of the homework

--- JavaScript_cond_and_loops/task3.js
+++ JavaScript_cond_and_loops/task3.js
@@ -3,15 +3,42 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 
-readline.question("Please enter a number:", number => {
-    console.log(`The multiplication table of ${number} is:`);
-    multiplicationTable(number);
-    readline.close();
-})
 
-function multiplicationTable(number) {
+function multiplicationTableFor(number) {
     for (let i = 1; i <= 10; i++) {
         console.log(`${number} * ${i} = ${number * i}`);
     }
 }
 
+
+function multiplicationTableWhile(number) {
+    let i = 1;
+    while (i <= 10) {
+        console.log(`${number} * ${i} = ${number * i}`);
+        i++;
+    }
+}
+
+
+function chooseFunction() {
+    readline.question("Do you want to use a while loop or a for loop? ", choose => {
+        switch (choose.toLowerCase()) {
+            case "while":
+                readline.question("Please enter a number: ", number => {
+                    console.log(`The multiplication table of ${number} is:`);
+                    multiplicationTableWhile(number);
+                    readline.close();
+                });
+                break;
+            case "for":
+                readline.question("Please enter a number: ", number => {
+                    console.log(`The multiplication table of ${number} is:`);
+                    multiplicationTableFor(number);
+                    readline.close();
+                });
+                break;
+        }
+    });
+}
+
+chooseFunction();
