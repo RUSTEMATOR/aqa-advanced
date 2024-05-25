2037342b2e90603d5baa9d43fd0b6a5617873fe6
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-05-25 18:32:32 +0300

    completed tasks from  the 7-th lesson

---
+++ advanced_function_usage/task1.js
@@ -0,0 +1,21 @@
+
+
+function handleNum(num, callback1, callback2){
+    if (num % 2 == 0){
+        callback1(num);
+    }
+    else {
+        callback2(num);
+    }
+}
+
+
+const handleEven = () => console.log("The number is even");
+
+
+const handleOdd = () => console.log("The number is odd");
+
+
+handleNum(10, handleEven, handleOdd);
+
+handleNum(3, handleEven, handleOdd);
---
+++ advanced_function_usage/task3.js
@@ -0,0 +1,41 @@
+function devide(numerator, denominator){
+    validator(numerator, denominator);
+    return numerator / denominator;
+} 
+
+function validator(numerator, denominator){
+    if (denominator === 0 || isNaN(denominator) || isNaN(numerator)) {
+        throw new Error('Invalid arguments');
+    }
+}
+
+
+try {
+    console.log(devide(10, 2)); 
+} catch (error) {
+    console.error("An error occurred:", error.message);
+}
+
+try {
+    console.log(devide(10, 0));
+} catch (error) {
+    console.error("An error occurred:", error.message);
+}
+
+try {
+    console.log(devide("fa", 2)); 
+} catch (error) {
+    console.error("An error occurred:", error.message);
+}
+
+try {
+    console.log(devide(10, "fa")); 
+} catch (error) {
+    console.error("An error occurred:", error.message);
+}
+
+try {
+    console.log(devide("fa", "fa")); 
+} catch (error) {
+    console.error("An error occurred:", error.message);
+}
