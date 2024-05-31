346acf93725bf7147ae7a3faa2a07b9c23a03abe
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-05-31 17:22:17 +0300

    completed the 8-th homework

---
+++ Arrays_in_JS/task3.js
@@ -0,0 +1,7 @@
+const numbers = [10, 20, 30, 40, 50]
+
+
+let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+
+console.log(sum)
---
+++ Arrays_in_JS/task6.js
@@ -0,0 +1,6 @@
+const numbersList = [1,10,14,2,4,5,43,34]
+
+let sortedList = Array.from(numbersList).sort((a, b) => a - b)
+
+
+console.log(sortedList)
