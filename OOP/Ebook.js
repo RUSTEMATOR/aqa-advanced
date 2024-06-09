227d30346fbf25499f2bad5d5b9ced2b7732d775
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-06-09 22:10:13 +0300

    Completed the OOP tasks

---
+++ OOP/Ebook.js
@@ -0,0 +1,16 @@
+import {Book} from "./Book.js";
+
+export class Ebook extends Book{
+        constructor(title, author, issueYear, fileExtension){
+            super(title, author, issueYear);
+            this.fileExtension = fileExtension;
+    }
+    
+    static makingEbook(book, fileExtension){
+        return new Ebook(book.title, book.author, book.issueYear, fileExtension);
+    }
+    printInfo(){
+        console.log(this.title, this.author, this.issueYear, this.fileExtension);
+    }
+}
+
