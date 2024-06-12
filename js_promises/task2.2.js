d89063c26457cdfea5fe58f8bedc579c83e86d20
Rustem Samoilenko <samoilenkorustem@gmail.com>
2024-06-12 23:33:23 +0300

    fixed 2nd task

--- js_promises/task2.2.js
+++ js_promises/task2.2.js
@@ -2,9 +2,17 @@
 
 
 async function fetchData() {
-    const response = await fetch('swapi.dev/api/people/14/');
+    const response = await fetch('https://swapi.dev/api/people/14');
     const data = await response.json();
-    console.log(data.starships); 
+    const starships = data.starships;
+
+
+    const starshipNames = await Promise.all(starships.map(async (starshipLink) => {
+        const starshipResponse = await fetch(starshipLink);
+        const starshipData = await starshipResponse.json();
+        return starshipData.name;
+    }))
+    console.log(starshipNames);
 }
 
 fetchData();
